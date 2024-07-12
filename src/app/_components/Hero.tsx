e2d4e09f8c350a139a51f0b80ca9aaf7ff07fa21
anoncollab12 <anoncollab12@protonmail.com>
2024-07-11 21:19:35 -0600

    Landing page draft

---
+++ src/app/_components/Hero.tsx
@@ -0,0 +1,60 @@
+export default function Hero() {
+  return (
+    <div className="relative h-screen w-full overflow-hidden">
+      <div className="absolute inset-0 -left-[25%] -top-[25%] h-[150%] w-[150%]">
+        <iframe
+          src="https://www.youtube.com/embed/0M1C9yEzplI?autoplay=1&mute=1&loop=1&controls=0&showinfo=0&rel=0&iv_load_policy=3&playlist=0M1C9yEzplI"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          className="h-full w-full object-cover"
+          title="Background video"
+        />
+      </div>
+
+      <div className="relative flex h-full flex-col items-center justify-center bg-black bg-opacity-50 p-4 text-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Membresía holística
+          </h1>
+          <p className="mb-8 text-lg font-normal text-gray-500 sm:px-16 lg:text-xl xl:px-48 dark:text-gray-400">
+            Texto
+          </p>
+          <div className="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16">
+            <a
+              href="#"
+              className="bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-900 inline-flex items-center justify-center rounded-lg px-5 py-3 text-center text-base font-medium text-white focus:ring-4"
+            >
+              Conoce más
+              <svg
+                className="-mr-1 ml-2 h-5 w-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+            <a
+              href="#"
+              className="inline-flex items-center justify-center rounded-lg border border-gray-300 px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            >
+              <svg
+                className="-ml-1 mr-2 h-5 w-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
+              </svg>
+              Ir a los cursos
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/_components/NavBar.tsx
@@ -0,0 +1,133 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <header className="sticky top-0 z-10">
+      <nav className=" border-gray-200 bg-white px-4 py-2.5 lg:px-6">
+        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
+          <a href="#" className="flex items-center">
+            <Image
+              src="/assets/cilcaLogo.png"
+              className="sm:h-15 sm:w-15"
+              width={60}
+              height={60}
+              alt="CILCA Logo"
+              unoptimized={true}
+            />
+            <span className="self-center whitespace-nowrap text-xl font-semibold text-fuchsia-700">
+              CILCA
+            </span>
+          </a>
+          <div className="flex items-center lg:order-2">
+            <a
+              href="#"
+              className="mr-2 rounded-lg px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-50 focus:outline-none focus:ring-4 focus:ring-gray-300 lg:px-5 lg:py-2.5"
+            >
+              Registro
+            </a>
+            <a
+              href="#"
+              className="bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 mr-2 rounded-lg px-4 py-2 text-sm font-medium focus:outline-none focus:ring-4 lg:px-5 lg:py-2.5"
+            >
+              Inicia sesión
+            </a>
+            <button
+              onClick={toggleMenu}
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              className="ml-1 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 lg:hidden"
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {isMenuOpen ? (
+                <svg
+                  className="h-6 w-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  className="h-6 w-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              )}
+            </button>
+          </div>
+          <div
+            className={`${
+              isMenuOpen ? "block" : "hidden"
+            } w-full items-center justify-between lg:order-1 lg:flex lg:w-auto`}
+            id="mobile-menu-2"
+          >
+            <ul className="mt-4 flex flex-col font-medium lg:mt-0 lg:flex-row lg:space-x-8">
+              <li>
+                <a
+                  href="#"
+                  className="bg-primary-700 lg:text-primary-700 block rounded  py-2 pl-3 pr-4 lg:bg-transparent lg:p-0"
+                  aria-current="page"
+                >
+                  Cursos
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 lg:border-0 lg:p-0 lg:hover:bg-transparent"
+                >
+                  Membresía
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 lg:border-0 lg:p-0 lg:hover:bg-transparent"
+                >
+                  Equipo
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 lg:border-0 lg:p-0 lg:hover:bg-transparent"
+                >
+                  Beca un niño
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 lg:border-0 lg:p-0 lg:hover:bg-transparent"
+                >
+                  Contacto
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
