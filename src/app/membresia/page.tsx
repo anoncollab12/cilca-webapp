6faba91cea8edde044f59e50d123a2d8acfe38a8
anoncollab12 <anoncollab12@protonmail.com>
2024-07-27 08:32:36 -0600

    Add membresia page

---
+++ src/app/membresia/page.tsx
@@ -0,0 +1,252 @@
+export default function Membresia() {
+  return (
+    <section className="bg-purple-50 dark:bg-gray-900">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+        <div className="mx-auto mb-8 max-w-screen-md text-center lg:mb-12">
+          <h2 className="mb-4 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
+            Diferentes modalidades de membresía
+          </h2>
+          <p className="mb-5 font-light text-gray-500 dark:text-gray-400 sm:text-xl">
+            Selecciona el plan ideal para ti.
+          </p>
+        </div>
+        <div className="space-y-8 sm:gap-6 lg:grid lg:grid-cols-3 lg:space-y-0 xl:gap-10">
+          <div className="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8">
+            <h3 className="mb-4 text-2xl font-semibold">Inicial</h3>
+            <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
+              Buena opción para para comenzar a aprender un curso a la vez.
+            </p>
+            <div className="my-8 flex items-baseline justify-center">
+              <span className="mr-2 text-5xl font-extrabold">$40</span>
+              <span className="text-gray-500 dark:text-gray-400">/mes</span>
+            </div>
+            <ul role="list" className="mb-8 space-y-4 text-left">
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>1 curso al mes</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Ventaja 2</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Ventaja 3</span>
+              </li>
+            </ul>
+            <a
+              href="#"
+              className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-200 dark:focus:ring-primary-900 rounded-lg px-5 py-2.5 text-center text-sm font-medium focus:ring-4"
+            >
+              Seleccionar
+            </a>
+          </div>
+          <div className="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8">
+            <h3 className="mb-4 text-2xl font-semibold">Estándar</h3>
+            <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
+              Excelente para aprender de distintos cursos al mes.
+            </p>
+            <div className="my-8 flex items-baseline justify-center">
+              <span className="mr-2 text-5xl font-extrabold">$99</span>
+              <span className="text-gray-500 dark:text-gray-400">/mes</span>
+            </div>
+            <ul role="list" className="mb-8 space-y-4 text-left">
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>3 cursos al mes</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Ventaja 2</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Ventaja 3</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Ventaja 4</span>
+              </li>
+            </ul>
+            <a
+              href="#"
+              className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-200 dark:focus:ring-primary-900 rounded-lg px-5 py-2.5 text-center text-sm font-medium"
+            >
+              Seleccionar
+            </a>
+          </div>
+          <div className="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8">
+            <h3 className="mb-4 text-2xl font-semibold">Premium</h3>
+            <p className="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
+              La mejor para aprender sin límites.
+            </p>
+            <div className="my-8 flex items-baseline justify-center">
+              <span className="mr-2 text-5xl font-extrabold">$199</span>
+              <span className="text-gray-500 dark:text-gray-400">/mes</span>
+            </div>
+            <ul role="list" className="mb-8 space-y-4 text-left">
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Cursos ilimitados</span>
+              </li>{" "}
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Ventaja 2</span>
+              </li>{" "}
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Ventaja 3</span>
+              </li>{" "}
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Ventaja 4</span>
+              </li>{" "}
+              <li className="flex items-center space-x-3">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Ventaja 5</span>
+              </li>
+            </ul>
+            <a
+              href="#"
+              className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-200 dark:focus:ring-primary-900 rounded-lg px-5 py-2.5 text-center text-sm font-medium"
+            >
+              Seleccionar
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
