5dbfa6e5af7f1456da4ac4510ce4d094dcf91c6e
anoncollab12 <anoncollab12@protonmail.com>
2024-08-02 09:50:28 -0600

    force dynamic cursos page

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function Cursos() {
   const post = await db.query.posts.findMany();
   console.log(post);
