3b227a8773f005c995376a7c095d38bb7c147170
anoncollab12 <anoncollab12@protonmail.com>
2024-09-08 16:59:50 -0600

    refactor: Improved course query implementation

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -2,6 +2,8 @@
 import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
 import { type cursoCategoriaeEnum, cursos, modulos } from "./db/schema";
+import { courseFormSchema } from "~/lib/validations";
+import { z } from "zod";
 
 export async function getCourse(id: number) {
   const curso = await db.query.cursos.findFirst({
@@ -38,24 +40,22 @@ export async function getAuthorName(id: string) {
 
 export async function insertFormCourse(formData: FormData) {
   try {
-    const newCourse = {
-      authorId: "123" as string,
-      name: formData.get("name") as string,
-      urlThumbnail: formData.get("urlThumbnail") as string,
-      urlTrailer: formData.get("urlTrailer") as string,
-      description: formData.get("description") as string,
-      category: "Arte" as (typeof cursoCategoriaeEnum.enumValues)[number],
-      price: formData.get("price") as string,
-    };
+    const parsedData = Object.fromEntries(formData.entries());
+    const validatedData = courseFormSchema.parse(parsedData);
 
     const insertedCourse = await db
       .insert(cursos)
-      .values(newCourse)
+      .values(validatedData)
       .returning();
     return insertedCourse[0];
   } catch (error) {
-    console.error("Error inserting course:", error);
-    throw error;
+    if (error instanceof z.ZodError) {
+      console.error("Validation Error:", error.errors);
+      throw new Error(error.errors.map((e) => e.message).join(", "));
+    } else {
+      console.error("Error inserting course:", error);
+      throw error;
+    }
   }
 }
 
