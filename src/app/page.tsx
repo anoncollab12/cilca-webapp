aee388ca6d149deb143a3a8c20c622cd19984f17
Aminadab Cruz <aminadabcruz@proton.me>
2024-09-13 22:09:41 -0600

    Agregar info en la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CilcaInfo from "./_components/CilcaInfo";
 import Hero from "./_components/Hero";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <CilcaInfo />
     </>
   );
 }
